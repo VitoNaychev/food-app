fba0dc9e5d4993746d1db1641740dad1e3cec2a6
Viktor Naychev <vitonaychev@gmail.com>
2023-10-30 10:24:32 +0200

    add router server tests

--- router_server.go
+++ router_server.go
@@ -1,1 +1,21 @@
 package bt_customer_svc
+
+import (
+	"net/http"
+)
+
+type RouterServer struct {
+	http.Handler
+}
+
+func InitRouterServer(customerServer http.Handler, addressServer http.Handler) *RouterServer {
+	routerServer := new(RouterServer)
+
+	router := http.NewServeMux()
+	router.Handle("/customer/", customerServer)
+	router.Handle("/customer/address/", addressServer)
+
+	routerServer.Handler = router
+
+	return routerServer
+}
---
+++ router_server_test.go
@@ -0,0 +1,81 @@
+package bt_customer_svc
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var customerHandlerMessage = "Hello from customer handler"
+var addressHandlerMessage = "Hello from address handler"
+
+func fakeCustomerHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(customerHandlerMessage))
+}
+
+func fakeAddressHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(addressHandlerMessage))
+
+}
+
+func TestRouterServer(t *testing.T) {
+	fakeCustomerServer := http.HandlerFunc(fakeCustomerHandler)
+	fakeAddressServer := http.HandlerFunc(fakeAddressHandler)
+
+	routerServer := InitRouterServer(fakeCustomerServer, fakeAddressServer)
+
+	t.Run("routes requests to the customer server", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/customer/", nil)
+		response := httptest.NewRecorder()
+
+		routerServer.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+
+		want := customerHandlerMessage
+		got := getMessageFromBody(response.Body)
+
+		assertHandlerMessage(t, got, want)
+	})
+
+	t.Run("routes requests to the address server", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/customer/address/", nil)
+		response := httptest.NewRecorder()
+
+		routerServer.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+
+		want := addressHandlerMessage
+		got := getMessageFromBody(response.Body)
+
+		assertHandlerMessage(t, got, want)
+	})
+
+	t.Run("returns Not Found on unknown path", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/unknown/path", nil)
+		response := httptest.NewRecorder()
+
+		routerServer.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusNotFound)
+	})
+}
+
+func getMessageFromBody(body io.Reader) string {
+	buf := bytes.NewBuffer([]byte{})
+	buf.ReadFrom(body)
+	return buf.String()
+}
+
+func assertHandlerMessage(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got message %q want %q", got, want)
+	}
+}
