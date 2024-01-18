9fafb09f5b8a980e4f20b3a079708f70a7320be5
Viktor Naychev <vitonaychev@gmail.com>
2024-01-18 12:08:52 +0200

    docker compose for kafka broker used across services

--- restaurant-svc/cmd/main.go
+++ restaurant-svc/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/VitoNaychev/food-app/appenv"
@@ -19,6 +20,8 @@ func main() {
 		Dbuser: os.Getenv("POSTGRES_USER"),
 		Dbpass: os.Getenv("POSTGRES_PASSWORD"),
 		Dbname: os.Getenv("POSTGRES_DB"),
+
+		KafkaBrokers: strings.Split(os.Getenv("KAFKA_BROKERS"), ","),
 	}
 
 	service.Run(context.Background(), env, ":8080")
