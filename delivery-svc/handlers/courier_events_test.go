af64018f8e4e995f2866a95270881147ff687d70
Viktor Naychev <vitonaychev@gmail.com>
2024-01-29 15:36:32 +0200

    missing file from last commit

--- delivery-svc/handlers/courier_events_test.go
+++ delivery-svc/handlers/courier_events_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/VitoNaychev/food-app/delivery-svc/handlers"
+	"github.com/VitoNaychev/food-app/delivery-svc/models"
 	"github.com/VitoNaychev/food-app/delivery-svc/stubs"
 	"github.com/VitoNaychev/food-app/delivery-svc/testdata"
 	"github.com/VitoNaychev/food-app/events"
@@ -13,25 +14,32 @@ import (
 
 func TestCourierEventHandler(t *testing.T) {
 	courierStore := &stubs.StubCourierStore{}
+	locationStore := &stubs.StubLocationStore{}
 
-	eventHandler := handlers.NewCourierEventHandler(courierStore)
+	eventHandler := handlers.NewCourierEventHandler(courierStore, locationStore)
 
-	t.Run("creates courier on COURIER_CREATED_EVENT", func(t *testing.T) {
-		want := testdata.VolenCourier
+	t.Run("creates courier and location on COURIER_CREATED_EVENT", func(t *testing.T) {
+		wantCourier := testdata.VolenCourier
+		wantLocation := models.Location{
+			CourierID: wantCourier.ID,
+			Lat:       0.0,
+			Lon:       0.0,
+		}
 
 		payload := svcevents.CourierCreatedEvent{
-			ID:   want.ID,
-			Name: want.Name,
+			ID:   wantCourier.ID,
+			Name: wantCourier.Name,
 		}
 		event := events.NewTypedEvent(svcevents.COURIER_CREATED_EVENT_ID, 1, payload)
 
 		err := eventHandler.HandleCourierCreatedEvent(event)
 
 		testutil.AssertNoErr(t, err)
-		testutil.AssertEqual(t, courierStore.CreatedCourier, want)
+		testutil.AssertEqual(t, courierStore.CreatedCourier, wantCourier)
+		testutil.AssertEqual(t, locationStore.CreatedLocation, wantLocation)
 	})
 
-	t.Run("deletes courier on COURIER_DELETED_EVENT", func(t *testing.T) {
+	t.Run("deletes courier and location on COURIER_DELETED_EVENT", func(t *testing.T) {
 		want := testdata.VolenCourier
 
 		payload := svcevents.CourierDeletedEvent{
@@ -43,5 +51,6 @@ func TestCourierEventHandler(t *testing.T) {
 
 		testutil.AssertNoErr(t, err)
 		testutil.AssertEqual(t, courierStore.DeletedCourierID, want.ID)
+		testutil.AssertEqual(t, locationStore.DeletedLocationCourierID, want.ID)
 	})
 }
