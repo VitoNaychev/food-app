f88b1528c04fe8c86fa20da5c6700c4db6f255a4
Viktor Naychev <vitonaychev@gmail.com>
2024-01-31 22:58:02 +0200

    remove delivery time field

--- order-svc/testdata/orders.go
+++ order-svc/testdata/orders.go
@@ -1,8 +1,6 @@
 package testdata
 
 import (
-	"time"
-
 	"github.com/VitoNaychev/food-app/order-svc/models"
 )
 
@@ -12,7 +10,6 @@ var (
 		CustomerID:      1,
 		RestaurantID:    1,
 		Total:           13.12,
-		DeliveryTime:    time.Date(2024, 2, 12, 18, 30, 00, 00, time.UTC),
 		PickupAddress:   1,
 		DeliveryAddress: 2,
 		Status:          models.APPROVAL_PENDING,
@@ -22,7 +19,6 @@ var (
 		CustomerID:      1,
 		RestaurantID:    1,
 		Total:           19.22,
-		DeliveryTime:    time.Date(2022, 2, 12, 18, 30, 00, 00, time.UTC),
 		PickupAddress:   1,
 		DeliveryAddress: 3,
 		Status:          models.COMPLETED,
@@ -32,7 +28,6 @@ var (
 		CustomerID:      2,
 		RestaurantID:    1,
 		Total:           14.42,
-		DeliveryTime:    time.Date(2022, 2, 12, 17, 00, 00, 00, time.UTC),
 		PickupAddress:   1,
 		DeliveryAddress: 4,
 		Status:          models.COMPLETED,
